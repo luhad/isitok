dae193987dbae1c323d11b34c1e4b555a67a6ca3
VitorSF <sfz.vitor@gmail.com>
2019-01-04 14:18:00 -0200

    model adjustments

--- app/back/models/panelItem.js
+++ app/back/models/panelItem.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose')
 
 const panelItemSchema = mongoose.Schema({
-    panel: { type: Schema.Types.ObjectId, ref: 'panel' },
+    panel_id: { type: Schema.Types.ObjectId, ref: 'panel' },
     name: { type: String, required: true },
+    description: { type: String, required: true },
     api_url: { type: String, required: true },
     api_authorization: { type: String, required: true },
     type: { type: String, required: true },
-    last_update_data: { type: String, required: false },
-    last_update_data_list: { type: String, required: false },
-    last_update_date: { type: String, required: false },
+    last_update_data: { type: String },
+    last_update_data_list: [
+        {
+            name: { type: String, required: true },
+            description: { type: String },
+            value: { type: String, required: true },
+            has_problem: { type: Boolean, required: true },
+        }
+    ],
+    last_update_date: { type: Date, required: Date.now },
     last_update_response: { type: String, required: false },
     last_update_message: { type: String, required: false },
-    grid_size: { type: String, required: true },
 })
-
-panelItemSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('PanelItem', panelItemSchema, 'panel_item')
